614c8b80de2c9f61c6e49483a327feb4a4389653
Keegan Rush <thecodedself@gmail.com>
2017-01-04 18:19:54 +0200

    Simple POC of Google Places searching

--- GooglePlacesPOC/ViewController.swift
+++ GooglePlacesPOC/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class ViewController: UIViewController {
-
+    
+    var southAfricanRegionAutocompleteFilter: GMSAutocompleteFilter {
+        let autocompleteFilter = GMSAutocompleteFilter()
+        autocompleteFilter.type = .region
+        autocompleteFilter.country = "ZA"
+        return autocompleteFilter
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        GMSPlacesClient.provideAPIKey(<#Key goes here#>)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Present the Autocomplete view controller when the button is pressed.
+    @IBAction func autocompleteClicked(_ sender: UIButton) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        autocompleteController.autocompleteFilter = southAfricanRegionAutocompleteFilter
+        present(autocompleteController, animated: true, completion: nil)
     }
-
-
+    
 }
 
+extension ViewController: GMSAutocompleteViewControllerDelegate {
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name)")
+        print("Place address: \(place.formattedAddress)")
+        print("Place attributions: \(place.attributions)")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Simple POC of searching for cities or suburbs with Google Places on iOS. Uses Swift 3.
